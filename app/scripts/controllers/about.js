9bfa90c096d4e19d93cc91eefb0d712e3358d8a0
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2016-02-18 17:17:12 -0500

    Creation of the wonderful project
    Made by Jean-Sébastien Perreault.
    Student number: 1432676

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projetEcoleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the projetEcoleApp
+ */
+angular.module('projetEcoleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projetEcoleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projetEcoleApp
+ */
+angular.module('projetEcoleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
