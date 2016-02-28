5e30f91280328ddb0d4fc8f56b4de77072dac001
Jean-Sébastien Perreault <jean-sebperro@outlook.com>
2016-02-28 18:34:36 -0500

    Created the page view and linked the navbar
    No tests were made
    Fields validation done.
    Fields input done.
    Linking the navbar done.
    Need to make the tests

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,7 +31,19 @@ angular
         controller: 'AboutCtrl',
         controllerAs: 'about'
       })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .controller('HeaderController',function HeaderController($scope, $location) 
+	{ 
+		$scope.isActive = function (viewLocation) { 
+			return viewLocation === $location.path();
+		};
+	});
+  
